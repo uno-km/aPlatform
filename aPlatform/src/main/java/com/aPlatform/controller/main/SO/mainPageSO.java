2893feadb52c30cdf8e93da3abd5cb887d1a538c
zhfld <zhfld@LAPTOP-AFG5HTSP>
2022-01-06 22:26:03 +0900

    create loadData

--- aPlatform/src/main/java/com/aPlatform/controller/main/SO/mainPageSO.java
+++ aPlatform/src/main/java/com/aPlatform/controller/main/SO/mainPageSO.java
@@ -6,28 +6,40 @@ import java.util.Locale;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping(value = "/")
-public class mainPageSO {
+public class mainPageSO
+{
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	public String home(Locale locale, Model model)
+	{
 		return "index";
 	}
 
 	@RequestMapping(value = "/favicon.ico", method = RequestMethod.GET)
+	public void favicon(HttpServletRequest request, HttpServletResponse reponse)
+	{
 
-	public void favicon(HttpServletRequest request, HttpServletResponse reponse) {
-
-		try {
+		try
+		{
 			reponse.sendRedirect("/resources/assets/favicon.ico");
-		} catch (IOException e) {
+		}
+		catch (IOException e)
+		{
 			e.printStackTrace();
 		}
+	}
+
+	@PostMapping(value = "loadservice")
+	public void serviceList()
+	{
+		System.out.println("check");
 	}
 }
