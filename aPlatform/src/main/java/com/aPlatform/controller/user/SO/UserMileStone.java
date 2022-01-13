01d3e00368e71cdf171d7371dd5717f3b9842092
uno-kim <zhfldk014745@naver.com>
2022-01-13 20:18:29 +0900

    add spring security

---
+++ aPlatform/src/main/java/com/aPlatform/controller/user/SO/UserMileStone.java
@@ -0,0 +1,28 @@
+package com.aPlatform.controller.user.SO;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@Controller
+@RequestMapping(value = "/sample")
+public class UserMileStone
+{
+	@GetMapping("/all")
+	public void doAll()
+	{
+		log.info("logined member");
+	}
+	@GetMapping("/member")
+	public void doMember()
+	{
+		log.info("member member");
+	}
+	@GetMapping("/manager")
+	public void doManager()
+	{
+		log.info("manager member");
+	}
+}
