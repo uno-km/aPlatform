f0e798e82654a04b9cb34f02e1c0ab7c39c8b697
uno-kim <zhfldk014745@naver.com>
2022-01-16 23:20:17 +0900

    success back end position at login

--- aPlatform/src/main/java/com/aPlatform/controller/user/VO/UserinfoVO.java
+++ aPlatform/src/main/java/com/aPlatform/controller/user/VO/UserinfoVO.java
@@ -1,15 +1,17 @@
 package com.aPlatform.controller.user.VO;
 
+import java.util.List;
+
 import lombok.Data;
 
-@Data
-public class UserinfoVO
+@Datapublic class UserinfoVO
 {
 	private String user_id;
 	private String user_password;
 	private String user_email;
 	private String user_name;
 	private String user_phonenum;
-	private String user_auth;
+	// private String user_auth;
+	private List<UserinfoVO> user_auth;
 	private String user_birth;
 }
