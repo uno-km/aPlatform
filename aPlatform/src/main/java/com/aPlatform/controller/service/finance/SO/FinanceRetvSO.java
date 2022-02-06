76ddcd5a6663cfb18d0ce4ca29cfee265c3b04ed
uno-kim <zhfldk014745@naver.com>
2022-02-06 21:02:55 +0900

    add finance apu

--- aPlatform/src/main/java/com/aPlatform/controller/service/finance/SO/FinanceRetvSO.java
+++ aPlatform/src/main/java/com/aPlatform/controller/service/finance/SO/FinanceRetvSO.java
@@ -1,6 +1,16 @@
 package com.aPlatform.controller.service.finance.SO;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/service/finance")
 public class FinanceRetvSO
 {
-
+	@GetMapping(value = "/main")
+	private String reternMainPage()
+	{
+		return "main";
+	}
 }
