ac36236f20e9f501831c0bcbfa0c55d2f1d3cef1
uno-kim <zhfldk014745@naver.com>
2022-02-17 00:13:38 +0900

    get data from naver.fin.com

--- aPlatform/src/main/webapp/resources/js/service/finance.js
+++ aPlatform/src/main/webapp/resources/js/service/finance.js
@@ -6,9 +6,11 @@ var kospiImage='';
 var kosdaqIndex='';
 var kosdaqBuyer='';
 var kosdaqImage='';
+var rankDataMC = '';
 
 window.addEventListener('load', function() {
 	getFindata();
+	getRankdata();
 	setKospiIndex();
 	setKosdaqIndex();
 	setKospiImage();
@@ -40,6 +42,23 @@ function getFindata() {
 	this.kosdaqImage= outData[5];
     this.nowFinData = outData;
 }
+function getRankdata() {
+	let outData='';
+	$.ajax({
+		type: 'GET',
+		url: '/service/finance/rank',
+		dataType: 'JSON', 
+		async: false,
+		contentType: 'application/json; charset=utf-8',
+		success: function (data) {
+		outData=data;
+	},
+		error: function () {
+		alert('통신실패!!');
+	}
+	});
+	this.rankDataMC =outData;
+}
 function setKospiIndex() {
 	const inputBody= document.getElementById('kospiIndex');
 	inputBody.innerText = this.kospiIndex;
@@ -60,11 +79,11 @@ function setKospiBuyer() {
 	let struct_div ="";
 	if(this.kospiBuyer!=null) {
 		struct_div	=	`	<div class='inner_kosdaqBuyer'>개인</div>
-							<div class='inner_kosdaqBuyer'>${this.kospiBuyer.kospi_buyer_ant}</div>
+							<div class='inner_kosdaqBuyer'>${this.kospiBuyer.kospi_ant}</div>
 							<div class='inner_kosdaqBuyer'>기관</div>
-							<div class='inner_kosdaqBuyer'>${this.kospiBuyer.kospi_buyer_org}</div>
+							<div class='inner_kosdaqBuyer'>${this.kospiBuyer.kospi_org}</div>
 							<div class='inner_kosdaqBuyer'>외국인</div>
-							<div class='inner_kosdaqBuyer'>${this.kospiBuyer.kospi_buyer_frg}</div>
+							<div class='inner_kosdaqBuyer'>${this.kospiBuyer.kospi_frg}</div>
 						`;
 		const inputBody = document.getElementById('kospiBuyer');
 		inputBody.innerHTML=struct_div;
@@ -77,11 +96,11 @@ function setKosdaqBuyer() {
 	if(this.kosdaqBuyer!=null) {
 		struct_div	=	`
 							<div class='inner_kosdaqBuyer'>개인</div>
-							<div class='inner_kosdaqBuyer'>${this.kosdaqBuyer.kosdaq_buyer_ant}</div>
+							<div class='inner_kosdaqBuyer'>${this.kosdaqBuyer.kosdaq_ant}</div>
 							<div class='inner_kosdaqBuyer'>기관</div>
-							<div class='inner_kosdaqBuyer'>${this.kosdaqBuyer.kosdaq_buyer_org}</div>
+							<div class='inner_kosdaqBuyer'>${this.kosdaqBuyer.kosdaq_org}</div>
 							<div class='inner_kosdaqBuyer'>외국인</div>
-							<div class='inner_kosdaqBuyer'>${this.kosdaqBuyer.kosdaq_buyer_frg}</div>
+							<div class='inner_kosdaqBuyer'>${this.kosdaqBuyer.kosdaq_frg}</div>
 						`;
 		const inputBody = document.getElementById('kosdaqBuyer');
 		inputBody.innerHTML=struct_div;
