94d72bc63760e3a82694208b7e6953c747cd74aa
uno-kim <zhfldk014745@naver.com>
2022-02-22 23:17:28 +0900

    tuning for speed and logic and visiblity

--- aPlatform/src/main/java/com/aPlatform/controller/service/finance/BO/FinanceRetvBO.java
+++ aPlatform/src/main/java/com/aPlatform/controller/service/finance/BO/FinanceRetvBO.java
@@ -1,6 +1,8 @@
 package com.aPlatform.controller.service.finance.BO;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,8 +15,18 @@ public class FinanceRetvBO
 {
 	@Autowired
 	FinanceDataMapper financeDataMapper;
-	public List<FinanceVO> getCode()
+	public List<FinanceVO> getCode(String name)
 	{
-		return financeDataMapper.getCode();
+		return financeDataMapper.getCode(name);
+	}
+	public Map<String, String> getCodeMap()
+	{
+		Map<String, String> outMap = new HashMap<>();
+		List<FinanceVO> inList = financeDataMapper.getAllCode();
+		for (int i = 0; i < inList.size(); i++)
+		{
+			outMap.put(inList.get(i).getFin_name(), inList.get(i).getFin_code());
+		}
+		return outMap;
 	}
 }
