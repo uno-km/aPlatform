0c5e275bbfda649d058389f2ff9ad852d1a368a4
uno-kim <zhfldk014745@naver.com>
2022-03-01 22:36:47 +0900

    it's success to get share's detail data

--- aPlatform/src/main/java/com/aPlatform/controller/service/finance/SO/FinanceSearchBOC.java
+++ aPlatform/src/main/java/com/aPlatform/controller/service/finance/SO/FinanceSearchBOC.java
@@ -5,20 +5,20 @@ import java.util.ArrayList;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.aPlatform.controller.service.finance.BO.FinanceDetailBO;
 import com.aPlatform.controller.service.finance.VO.FinanceDataMatrix;
 import com.aPlatform.controller.service.finance.model.GetURLInfo;
-
+@Service
 public class FinanceSearchBOC
 {
 	@Autowired
 	FinanceDetailBO financeDetailBO;
 
-	GetURLInfo getUrlInfo;
-
 	public Map<String, ArrayList<String>> getInfoDTL(String code) throws IOException
 	{
+		GetURLInfo getUrlInfo = new GetURLInfo();
 		FinanceDataMatrix financeDataMatrix = new FinanceDataMatrix(code);
 		financeDataMatrix.setPageDOCMapByString("infoDTL");
 		return getUrlInfo.getMapStringArrayList(financeDataMatrix, "infoDTL", "detail");
