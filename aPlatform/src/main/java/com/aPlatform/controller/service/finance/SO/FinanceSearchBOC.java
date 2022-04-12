76ffe16839136be9d133313cac921110d6281502
uno-kim <zhfldk014745@naver.com>
2022-04-12 16:53:21 +0900

    change get URLs HTML

--- aPlatform/src/main/java/com/aPlatform/controller/service/finance/SO/FinanceSearchBOC.java
+++ aPlatform/src/main/java/com/aPlatform/controller/service/finance/SO/FinanceSearchBOC.java
@@ -22,38 +22,37 @@ public class FinanceSearchBOC
 
 	public Object getInfo(String dataform, Map<String, String> map) throws Exception
 	{
-		if(dataform.contains("kospi") || dataform.contains("kosdaq") || dataform.contains("total"))
-		{
-			ArrayList<Map<String, String>> outArr = new ArrayList<Map<String, String>>();
-			this.financeDataMatrix.setPageDOC();
-			switch (dataform) {
-				case "kosqi" :
-					outArr.add((Map<String, String>) getUrlInfo.getUrlInfoObject(financeDataMatrix, "kospi", "index"));
-					outArr.add((Map<String, String>) getUrlInfo.getUrlInfoObject(financeDataMatrix, "kospi", "buyer"));
-					outArr.add((Map<String, String>) getUrlInfo.getUrlInfoObject(financeDataMatrix, "kospi", "image"));
-					return outArr;
-				case "kosdaq" :
-					outArr.add((Map<String, String>) getUrlInfo.getUrlInfoObject(financeDataMatrix, "kosdaq", "index"));
-					outArr.add((Map<String, String>) getUrlInfo.getUrlInfoObject(financeDataMatrix, "kosdaq", "buyer"));
-					outArr.add((Map<String, String>) getUrlInfo.getUrlInfoObject(financeDataMatrix, "kosdaq", "image"));
-					return outArr;
-				default : /* 전부 */
-					outArr.add((Map<String, String>) getUrlInfo.getUrlInfoObject(financeDataMatrix, "kospi", "index"));
-					outArr.add((Map<String, String>) getUrlInfo.getUrlInfoObject(financeDataMatrix, "kospi", "buyer"));
-					outArr.add((Map<String, String>) getUrlInfo.getUrlInfoObject(financeDataMatrix, "kospi", "image"));
-					outArr.add((Map<String, String>) getUrlInfo.getUrlInfoObject(financeDataMatrix, "kosdaq", "index"));
-					outArr.add((Map<String, String>) getUrlInfo.getUrlInfoObject(financeDataMatrix, "kosdaq", "buyer"));
-					outArr.add((Map<String, String>) getUrlInfo.getUrlInfoObject(financeDataMatrix, "kosdaq", "image"));
-					return outArr;
-			}
-		}
-		else
-		{
-			String url = map.get("url");
-			String pharseType = map.get("pharseType");
-			this.financeDataMatrix.setSearchUrlPharseType(map);
-			this.financeDataMatrix.setPageDOC(url);
-			return getUrlInfo.getUrlInfoObject(this.financeDataMatrix, url, pharseType);
+		switch (dataform) {
+			case "kospi" :/* 코스피 */
+				this.financeDataMatrix.setPageDOC("kospi");
+				ArrayList<Map<String, String>> kospiArr = new ArrayList<Map<String, String>>();
+				kospiArr.add((Map<String, String>) getUrlInfo.getUrlInfoObject(financeDataMatrix, "kospi", "index"));
+				kospiArr.add((Map<String, String>) getUrlInfo.getUrlInfoObject(financeDataMatrix, "kospi", "buyer"));
+				kospiArr.add((Map<String, String>) getUrlInfo.getUrlInfoObject(financeDataMatrix, "kospi", "image"));
+				return kospiArr;
+			case "kosdaq" :/* 코스피 */
+				this.financeDataMatrix.setPageDOC("kosdaq");
+				ArrayList<Map<String, String>> kosdaqArr = new ArrayList<Map<String, String>>();
+				kosdaqArr.add((Map<String, String>) getUrlInfo.getUrlInfoObject(financeDataMatrix, "kosdaq", "index"));
+				kosdaqArr.add((Map<String, String>) getUrlInfo.getUrlInfoObject(financeDataMatrix, "kosdaq", "buyer"));
+				kosdaqArr.add((Map<String, String>) getUrlInfo.getUrlInfoObject(financeDataMatrix, "kosdaq", "image"));
+				return kosdaqArr;
+			case "total" : /* 전부 */
+				this.financeDataMatrix.setPageDOC();
+				ArrayList<Map<String, String>> totalArr = new ArrayList<Map<String, String>>();
+				totalArr.add((Map<String, String>) getUrlInfo.getUrlInfoObject(financeDataMatrix, "kospi", "index"));
+				totalArr.add((Map<String, String>) getUrlInfo.getUrlInfoObject(financeDataMatrix, "kospi", "buyer"));
+				totalArr.add((Map<String, String>) getUrlInfo.getUrlInfoObject(financeDataMatrix, "kospi", "image"));
+				totalArr.add((Map<String, String>) getUrlInfo.getUrlInfoObject(financeDataMatrix, "kosdaq", "index"));
+				totalArr.add((Map<String, String>) getUrlInfo.getUrlInfoObject(financeDataMatrix, "kosdaq", "buyer"));
+				totalArr.add((Map<String, String>) getUrlInfo.getUrlInfoObject(financeDataMatrix, "kosdaq", "image"));
+				return totalArr;
+			default :
+				String url = map.get("url");
+				String pharseType = map.get("pharseType");
+				this.financeDataMatrix.setSearchUrlPharseType(map);
+				this.financeDataMatrix.setPageDOC(url);
+				return getUrlInfo.getUrlInfoObject(this.financeDataMatrix, url, pharseType);
 		}
 	}
 }
