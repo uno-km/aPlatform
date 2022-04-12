a6e19d64412412ca4767fdc271ef04c298a6ca4d
uno-kim <zhfldk014745@naver.com>
2022-04-12 16:36:05 +0900

    refactorying

--- aPlatform/src/main/java/com/aPlatform/controller/service/finance/SO/FinanceSearchBOC.java
+++ aPlatform/src/main/java/com/aPlatform/controller/service/finance/SO/FinanceSearchBOC.java
@@ -13,13 +13,16 @@ public class FinanceSearchBOC
 {
 	@Autowired
 	FinanceDetailBO financeDetailBO;
+	@Autowired
+	GetURLInfo getUrlInfo;
+	@Autowired
+	FinanceDataMatrix financeDataMatrix;
 
 	public Object getInfo(Map<String, String> map) throws Exception
 	{
-		GetURLInfo getUrlInfo = new GetURLInfo();
-		FinanceDataMatrix financeDataMatrix = new FinanceDataMatrix(map);
 		String url = map.get("url");
 		String pharseType = map.get("pharseType");
+		financeDataMatrix.setSearchUrlPharseType(map);
 		financeDataMatrix.setPageDOC(url);
 		return getUrlInfo.getUrlInfoObject(financeDataMatrix, url, pharseType);
 	}
