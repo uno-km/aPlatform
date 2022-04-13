d10eed356b9029c4e99b64fad6c4d305e0a04552
uno-kim <zhfldk014745@naver.com>
2022-04-13 21:09:03 +0900

    too many change logic

--- aPlatform/src/main/java/com/aPlatform/controller/service/finance/SO/FinanceSearchBOC.java
+++ aPlatform/src/main/java/com/aPlatform/controller/service/finance/SO/FinanceSearchBOC.java
@@ -1,6 +1,7 @@
 package com.aPlatform.controller.service.finance.SO;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,32 +21,20 @@ public class FinanceSearchBOC
 	@Autowired
 	FinanceDataMatrix financeDataMatrix;
 
-	public Object getInfo(String dataform, Map<String, String> map) throws Exception
+	public Object getInfo(final String dataform, Map<String, String> map) throws Exception
 	{
 		switch (dataform) {
 			case "kospi" :/* 코스피 */
-				this.financeDataMatrix.setPageDOC("kospi");
-				ArrayList<Map<String, String>> kospiArr = new ArrayList<Map<String, String>>();
-				kospiArr.add((Map<String, String>) getUrlInfo.getUrlInfoObject(financeDataMatrix, "kospi", "index"));
-				kospiArr.add((Map<String, String>) getUrlInfo.getUrlInfoObject(financeDataMatrix, "kospi", "buyer"));
-				kospiArr.add((Map<String, String>) getUrlInfo.getUrlInfoObject(financeDataMatrix, "kospi", "image"));
-				return kospiArr;
-			case "kosdaq" :/* 코스피 */
-				this.financeDataMatrix.setPageDOC("kosdaq");
-				ArrayList<Map<String, String>> kosdaqArr = new ArrayList<Map<String, String>>();
-				kosdaqArr.add((Map<String, String>) getUrlInfo.getUrlInfoObject(financeDataMatrix, "kosdaq", "index"));
-				kosdaqArr.add((Map<String, String>) getUrlInfo.getUrlInfoObject(financeDataMatrix, "kosdaq", "buyer"));
-				kosdaqArr.add((Map<String, String>) getUrlInfo.getUrlInfoObject(financeDataMatrix, "kosdaq", "image"));
-				return kosdaqArr;
+				this.financeDataMatrix.setPageDOC(dataform);
+				return getUrlInfo.getUrlInfoObject(financeDataMatrix, dataform, dataform);
+			case "kosdaq" :/* 코스닥 */
+				this.financeDataMatrix.setPageDOC(dataform);
+				return getUrlInfo.getUrlInfoObject(financeDataMatrix, dataform, dataform);
 			case "total" : /* 전부 */
 				this.financeDataMatrix.setPageDOC();
 				ArrayList<Map<String, String>> totalArr = new ArrayList<Map<String, String>>();
-				totalArr.add((Map<String, String>) getUrlInfo.getUrlInfoObject(financeDataMatrix, "kospi", "index"));
-				totalArr.add((Map<String, String>) getUrlInfo.getUrlInfoObject(financeDataMatrix, "kospi", "buyer"));
-				totalArr.add((Map<String, String>) getUrlInfo.getUrlInfoObject(financeDataMatrix, "kospi", "image"));
-				totalArr.add((Map<String, String>) getUrlInfo.getUrlInfoObject(financeDataMatrix, "kosdaq", "index"));
-				totalArr.add((Map<String, String>) getUrlInfo.getUrlInfoObject(financeDataMatrix, "kosdaq", "buyer"));
-				totalArr.add((Map<String, String>) getUrlInfo.getUrlInfoObject(financeDataMatrix, "kosdaq", "image"));
+				totalArr.addAll((Collection<? extends Map<String, String>>) getUrlInfo.getUrlInfoObject(financeDataMatrix, "kospi", "kospi"));
+				totalArr.addAll((Collection<? extends Map<String, String>>) getUrlInfo.getUrlInfoObject(financeDataMatrix, "kosdaq", "kosdaq"));
 				return totalArr;
 			default :
 				String url = map.get("url");
