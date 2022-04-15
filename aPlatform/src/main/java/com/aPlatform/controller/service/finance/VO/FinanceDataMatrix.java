f524ef402b1fb3f216b0b04a7f225357fbab00ab
uno-kim <zhfldk014745@naver.com>
2022-04-15 20:39:12 +0900

    이거 지워도 실행할까

--- aPlatform/src/main/java/com/aPlatform/controller/service/finance/VO/FinanceDataMatrix.java
+++ aPlatform/src/main/java/com/aPlatform/controller/service/finance/VO/FinanceDataMatrix.java
@@ -23,7 +23,7 @@ public class FinanceDataMatrix
 	}
 	public void setSearchUrlPharseType(Map<String, String> map)
 	{
-		setMarketURLMap();
+//		setMarketURLMap();
 		if(map.containsKey("code"))
 		{
 			marketURLMap.put(map.get("url"), "https://finance.naver.com/item/main.naver?code=" + map.get("code"));
