3c09cb818a7f8f036830a76bc4dfcdc08e570aca
uno-kim <zhfldk014745@naver.com>
2022-04-20 20:20:09 +0900

    change logic

--- aPlatform/src/main/java/com/aPlatform/controller/service/finance/VO/FinanceDataMatrix.java
+++ aPlatform/src/main/java/com/aPlatform/controller/service/finance/VO/FinanceDataMatrix.java
@@ -5,10 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.ibatis.session.SqlSession;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import com.aPlatform.mappers.FinanceDataMapper;
 
@@ -17,28 +15,27 @@ import lombok.Data;
 @Data
 public class FinanceDataMatrix
 {
-	private static FinanceDataMatrix financeDataMatrix = new FinanceDataMatrix();
-	private FinanceDataMatrix()
-	{
-	}
-	public static synchronized FinanceDataMatrix getInstance()
-	{
-		if(financeDataMatrix == null)
-		{
-			financeDataMatrix = new FinanceDataMatrix();
-		}
-		return financeDataMatrix;
-	}
+	// private static FinanceDataMatrix financeDataMatrix = new FinanceDataMatrix();
+	// private FinanceDataMatrix()
+	// {
+	// }
+	// public static synchronized FinanceDataMatrix getInstance()
+	// {
+	// if(financeDataMatrix == null)
+	// {
+	// financeDataMatrix = new FinanceDataMatrix();
+	// }
+	// return financeDataMatrix;
+	// }
 
 	private Map<String, Document> pageDOCMap = new HashMap<String, Document>();;
 	private Map<String, String> marketURLMap = new HashMap<String, String>();
 	private String[] innerArr = {"kospi" , "kosdaq" };
-	@Autowired
 	private FinanceDataMapper financeDataMapper;
 
 	public void setMarketURLMap(Map<String, String> map)
 	{
-		// setMarketURLMap();
+		setMarketURLMap();
 		if(map.containsKey("code"))
 		{
 			marketURLMap.put(map.get("url"), "https://finance.naver.com/item/main.naver?code=" + map.get("code"));
