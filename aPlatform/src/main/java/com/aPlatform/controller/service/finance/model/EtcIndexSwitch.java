8e85a6a8e528d8c50f7aebea93a7bf6cacfd54f4
uno-kim <zhfldk014745@naver.com>
2022-04-29 20:42:21 +0900

    :sparkles: add UrlMileStone and java file

---
+++ aPlatform/src/main/java/com/aPlatform/controller/service/finance/model/EtcIndexSwitch.java
@@ -0,0 +1,46 @@
+package com.aPlatform.controller.service.finance.model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import com.aPlatform.controller.service.finance.VO.FinanceDataMatrix;
+
+public class EtcIndexSwitch implements UrlFactory
+{
+	@Override
+	public Object excute(FinanceDataMatrix financeDataMatrix, Document doc, Elements contents, HashMap<String, String> outMap,
+			String[] parsingContainer, String market, String pharseType)
+
+	{
+		contents = doc.select(financeDataMatrix.getMarketURLMap().get(pharseType));
+		Map<String, ArrayList<String>> outListMap = new LinkedHashMap<>();
+		parsingContainer = contents.text().split(" ");
+		int rankCnt = 0;
+		for (int i = 0; i < parsingContainer.length; i++)
+		{
+			ArrayList<String> innerArr = new ArrayList<>();
+			for (int innerCnt = 0; innerCnt < 5; innerCnt++)
+			{
+				if(parsingContainer[i + innerCnt].equals("보합"))
+				{
+					innerArr.add("0");
+					innerArr.add(parsingContainer[i + innerCnt]);
+					innerArr.add(parsingContainer[i + innerCnt + 1]);
+					i--;
+					break;
+				}
+				innerArr.add(parsingContainer[i + innerCnt]);
+			}
+			outListMap.put(Integer.toString(rankCnt), innerArr);
+			rankCnt++;
+			i = i + 4;
+		}
+		return outListMap;
+	}
+
+}
