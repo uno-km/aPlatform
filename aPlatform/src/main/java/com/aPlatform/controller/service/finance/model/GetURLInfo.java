62d32f73cdd6c721b3b49ffac69fc34b36670d03
uno-kim <zhfldk014745@naver.com>
2022-05-04 22:41:42 +0900

    :wrench: create new method for all data

--- aPlatform/src/main/java/com/aPlatform/controller/service/finance/model/GetURLInfo.java
+++ aPlatform/src/main/java/com/aPlatform/controller/service/finance/model/GetURLInfo.java
@@ -1,4 +1,6 @@
 package com.aPlatform.controller.service.finance.model;
+import java.util.LinkedHashMap;
+
 import org.springframework.stereotype.Service;
 import com.aPlatform.controller.service.finance.VO.FinanceDataMatrix;
 @Service
@@ -7,5 +9,12 @@ public class GetURLInfo
 	public Object getUrlInfoObject(final FinanceDataMatrix financeDataMatrix, final String url, final String pharseType) throws Exception
 	{
 		return FinanceURL.pharsingURL(financeDataMatrix, url, pharseType);
+	}
+	public Object getUrlInfoAllObject(final FinanceDataMatrix financeDataMatrix, final String url, final String pharseType) throws Exception
+	{
+		LinkedHashMap<String, Object> outMap = new LinkedHashMap<String, Object>();
+		for (int i = 0; i < UrlMileStone.values().length; i++)
+			outMap.put("Str", FinanceURL.pharsingURL(financeDataMatrix, url, pharseType));
+		return outMap;
 	}
 }
