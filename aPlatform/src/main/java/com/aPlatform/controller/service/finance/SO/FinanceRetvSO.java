ece4c6e830940f9d4a64becca84ea9d0bad0ef7a
uno-kim <zhfldk014745@naver.com>
2022-05-12 00:34:25 +0900

    :memo: create new method

--- aPlatform/src/main/java/com/aPlatform/controller/service/finance/SO/FinanceRetvSO.java
+++ aPlatform/src/main/java/com/aPlatform/controller/service/finance/SO/FinanceRetvSO.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +34,11 @@ public class FinanceRetvSO
 	}
 	@GetMapping(value = "/codeAllMap")
 	public Map<String, String> getCodeMap(Model model) throws Exception
+	{
+		return financeRetvBOC.getCodeMap();
+	}
+	@PostMapping(value = "/getSendingData")
+	public Map<String, String> getSendingData(@RequestBody Map<String, Object> inputMap) throws Exception
 	{
 		return financeRetvBOC.getCodeMap();
 	}
