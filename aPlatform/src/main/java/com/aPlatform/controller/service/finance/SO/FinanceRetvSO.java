956444b860f16737bc3b21619f750697a9f8de78
uno-kim <zhfldk014745@naver.com>
2022-05-13 20:15:48 +0900

    :memo: add value for get excel DATA

--- aPlatform/src/main/java/com/aPlatform/controller/service/finance/SO/FinanceRetvSO.java
+++ aPlatform/src/main/java/com/aPlatform/controller/service/finance/SO/FinanceRetvSO.java
@@ -3,6 +3,8 @@ package com.aPlatform.controller.service.finance.SO;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.aPlatform.controller.service.finance.BOC.FinanceRetvBOC;
@@ -26,6 +29,13 @@ public class FinanceRetvSO
 	FinanceSearchBOC financeSearchBOC;
 	@Autowired
 	FinanceDataMatrix financeDataMatrix;
+
+	@PostMapping(value = "/excel")
+	private ResponseEntity<String> execlDataFileInsert(MultipartFile uploadFile)
+	{
+		
+		return new ResponseEntity<String>("200", HttpStatus.OK);
+	}
 	@GetMapping(value = "/main")
 	private ModelAndView reternMainPage(Model model)
 	// private String reternMainPage(Model model)
