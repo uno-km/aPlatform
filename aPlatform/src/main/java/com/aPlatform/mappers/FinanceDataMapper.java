90106bf04ee22da8ad809b58f2015fd7fa1349ec
uno-kim <zhfldk014745@naver.com>
2022-05-13 21:54:31 +0900

    :tada: add insert function and delete function

--- aPlatform/src/main/java/com/aPlatform/mappers/FinanceDataMapper.java
+++ aPlatform/src/main/java/com/aPlatform/mappers/FinanceDataMapper.java
@@ -9,7 +9,14 @@ import com.aPlatform.controller.service.finance.VO.FinanceVO;
 @Mapper
 public interface FinanceDataMapper
 {
-	public List<FinanceVO> getAllCode();
-	public List<FinanceVO> getMappingUrl();
-	public List<FinanceVO> getMappingPharse();
+	/* 전체 종목코드 호출 */
+	abstract public List<FinanceVO> getAllCode();
+	/* 주식정보를 가져올 url 정보를 가져온다. */
+	abstract public List<FinanceVO> getMappingUrl();
+	/* html을 파싱할 각 인자들을 가져온다 */
+	abstract public List<FinanceVO> getMappingPharse();
+	/* 종목최진화 / 단건저장 */
+	abstract public void insertSharesInfo(FinanceVO inVO);
+	/* 기존 전종목 모두 삭제 */
+	abstract public void deleteAllDataInShareTable();
 }
