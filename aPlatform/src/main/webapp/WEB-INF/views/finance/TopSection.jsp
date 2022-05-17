2521406d941d6ecda89632cad92da034b8b188f9
uno-kim <zhfldk014745@naver.com>
2022-05-17 21:16:02 +0900

    :tada: add file tag for upload

--- aPlatform/src/main/webapp/WEB-INF/views/finance/TopSection.jsp
+++ aPlatform/src/main/webapp/WEB-INF/views/finance/TopSection.jsp
@@ -6,7 +6,10 @@
 	<div>
 		<div class='ShortView' style="display: flex; align-items: center;">
 			<div id="userInterest" style="width: 86%;"></div>
-			<div id="getStockItems" style="width: 14%;"></div>
+			<div id="getStockItems" style="width: 14%;">
+				<!-- 여기에 버튼생성됨 -->
+			</div>
+			<input type="file" id="excelFileUpload" style="visibility: hidden; display: none;">
 		</div>
 	</div>
 	<a href='#'>내 손안의 금융 - aFinance </a>
--- aPlatform/src/main/webapp/resources/js/service/fin/setUserInterestStocks.js
+++ aPlatform/src/main/webapp/resources/js/service/fin/setUserInterestStocks.js
@@ -5,17 +5,20 @@ function setInterestStockBnt(){
 	switch(auth) {
 		case "100" :
 			innerStruck.innerHTML = `
-			<button class="unoBnt unoBnt-middleBnt" type="button" id="">관심종목추가</button>
+			<button class="unoBnt unoBnt-middleBnt" type="button" id="addInterestStocks">관심종목추가</button>
 			`;
 			break;
 		case 'ROLE_MEMBER' :
 			innerStruck.innerHTML = `
-			<button class="unoBnt unoBnt-middleBnt" type="button" id="">종목최신화</button>
+			<button class="unoBnt unoBnt-middleBnt" type="button" id="uploadExcelFile">종목최신화</button>
 			`;
+			document.getElementById('uploadExcelFile').addEventListener('clcik', function(){
+				document.getElementById('excelFileUpload').click;
+			});
 			break;
 		default :
 			innerStruckinnerHTML = `
-			<button class="unoBnt unoBnt-middleBnt" type="button" id="">로그인</button>
+			<button class="unoBnt unoBnt-middleBnt" type="button" id="topSectionLogin">로그인</button>
 			`;
 			break;
 	}
