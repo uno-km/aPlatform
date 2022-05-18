02c32bc85196650c03e862690fe3aa5a432437fd
uno-kim <zhfldk014745@naver.com>
2022-05-18 20:32:11 +0900

    :art: add jsp

--- aPlatform/src/main/webapp/resources/js/service/fin/setUserInterestStocks.js
+++ aPlatform/src/main/webapp/resources/js/service/fin/setUserInterestStocks.js
@@ -24,24 +24,36 @@ function fn_uploaExcel() {
 	console.log('qwe');
 	let url = "excelUploadPopup";
 	let name = "_blank";
-	let setSizeOption = ['500','500','500','500'];
+	let setSizeOption = ['500','300','250','250'];
 	let setWebOption = [];
-	setPOPupOption(url,name,setOption);
+	setPOPupOption(url,name,setSizeOption,setWebOption);
 }
 function setPOPupOption(inUrl, inName, inSetSizeOption, inSetWebOption){
 	const sizeOptionList = ["width","height","top","left"];
 	const webOptionList = ["location","scrollbars","tollbar","menubar","status","fullscreen","resizeable"];
+	let width = "";
+	let height = "";
+	if(inSetSizeOption[2].length>1) {
+		width = inSetSizeOption[2];
+	}
+	if(inSetSizeOption[3].length>1) {
+		height = inSetSizeOption[3];
+	}
+	const popupWidth = (window.screen.width / 2) - (width / 2);
+	const popupHeight = (window.screen.height / 2) - (height / 2);
+	
 	const url = inUrl;
 	const name = inName;
 	let option = "";
 	if(inSetSizeOption.length!=0) {
-		for(let i = 0 ; i < inSetSizeOption.length;i++) {
-			option += sizeOptionList[i]+"="+inSetSizeOption[i];
-			if(i!=inSetSizeOption.lenght-1) 
-				option += ", ";
+		for(let i = 0 ; i < inSetSizeOption.length-2;i++) {
+			option += sizeOptionList[i]+"="+inSetSizeOption[i] + ", ";
 		}
+		option += sizeOptionList[2]+"="+popupWidth + ", ";
+		option += sizeOptionList[3]+"="+popupHeight;
 	}
     if(inSetWebOption.length!=0) {
+    	option += ", ";
     	for(let i = 0 ; i < inSetWebOption.length;i++) {
     		option += webOptionList[i]+"="+inSetWebOption[i];
     		if(i!=inSetWebOption.lenght-1) 
