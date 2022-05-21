e4112f296da52f4e9d9860a28a99aa00bc77e8d5
uno-kim <zhfldk014745@naver.com>
2022-05-21 21:50:10 +0900

    :art: add events

--- aPlatform/src/main/webapp/resources/js/service/fin/excelUploadPopup.js
+++ aPlatform/src/main/webapp/resources/js/service/fin/excelUploadPopup.js
@@ -1,4 +1,7 @@
 ;
+let regex = new RegExp("(.*?)\.(exe|sh|zip|alz)$");
+let maxSize = 5242880; //5MB
+
 window.addEventListener('load',excelPopupInit);
 var File = '';
 function excelPopupInit() {
@@ -9,10 +12,47 @@ function setEvents() {
 		document.getElementById('excelFileUpload').click();
 	});
 	document.getElementById('excelFileUpload').addEventListener('change',function(e){
-		alert(e);
+		 fileAttatchEvent(e);
 	});
 }
 
 function fileAttatchEvent(e) {
 	
 }
+
+function checkExtension(fileName, fileSize) {
+	if (fileSize >= maxSize) {
+		alert("파일 사이즈 초과");
+		return false;
+	}
+	if (regex.test(fileName)) {
+		alert("해당 종류의 파일은 업로드할 수 없습니다.");
+		return false;
+	}
+	return true;
+}
+$("#uploadBtn").on("click", function(e) {
+	var formData = new FormData();
+	var formData = new FormData();
+	var inputFile = $("input[name='uploadFile']");
+	var files = inputFile[0].files;
+	for (var i = 0; i < files.length; i++) {
+		if (!checkExtension(files[i].name, files[i].size)) {
+			return false;
+		}
+		formData.append("uploadFile", files[i]);
+	}
+	$.ajax({
+		url : '/uploadAjaxAction',
+		processData : false,
+		contentType : false,
+		data : formData,
+		type : 'POST',
+		dataType : 'json',
+		success : function(result) {
+			console.log(result);
+			showUploadedFile(result);
+			$(".uploadDiv").html(cloneObj.html());
+		}
+	}); //$.ajax
+});
