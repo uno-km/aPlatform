bee6b43525aba1b46a9e5b2c3d512dafcc8ffc82
uno-kim <zhfldk014745@naver.com>
2022-05-23 22:55:35 +0900

    :bug: re-modified functions

--- aPlatform/src/main/java/com/aPlatform/controller/service/finance/SO/FinanceRetvSO.java
+++ aPlatform/src/main/java/com/aPlatform/controller/service/finance/SO/FinanceRetvSO.java
@@ -1,20 +1,47 @@
 package com.aPlatform.controller.service.finance.SO;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.aPlatform.controller.service.finance.BOC.FinanceRetvBOC;
+import com.aPlatform.controller.service.finance.VO.FinanceDataMatrix;
 
 @RestController
 @RequestMapping(value = "/service/finance")
 public class FinanceRetvSO
 {
+	@Autowired
+	FinanceRetvBOC financeRetvBOC;
+	@Autowired
+	FinanceSearchBOC financeSearchBOC;
+	@Autowired
+	FinanceDataMatrix financeDataMatrix;
 
+	@GetMapping(value = "/main")
+	private ModelAndView reternMainPage(Model model)
+	// private String reternMainPage(Model model)
+	{
+		return financeRetvBOC.reternMainPage();
+	}
 	@GetMapping(value = "/excelUploadPopup")
 	private ModelAndView returnPopupExcelUpload()
 	{
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("finance/popup/excelUploadPopup");
 		return mv;
+	}
+	@GetMapping(value = "/{dataform}")
+	public Object getData(@PathVariable final String dataform, @RequestParam Map<String, Object> map) throws Exception
+	{
+		System.out.println("Client required " + dataform + " data...");
+		return financeSearchBOC.getInfo(financeDataMatrix, dataform, map);
 	}
 }
