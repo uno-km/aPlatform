a3536c3f6ae3dff8fd651a549509a3a87ffd050d
uno-kim <zhfldk014745@naver.com>
2022-05-24 00:27:02 +0900

    :tada: add file upload logic

--- aPlatform/src/main/java/com/aPlatform/controller/service/finance/BOC/FinanceRetvBOC.java
+++ aPlatform/src/main/java/com/aPlatform/controller/service/finance/BOC/FinanceRetvBOC.java
@@ -5,14 +5,18 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.aPlatform.controller.file.SO.FileUploadBO;
 import com.aPlatform.controller.service.finance.BO.FinanceRetvBO;
 @Service
 public class FinanceRetvBOC
 {
 	@Autowired
 	FinanceRetvBO financeRetvBO;
+	@Autowired
+	FileUploadBO fileUploadBO;
 
 	public ModelAndView reternMainPage()
 	{
@@ -25,7 +29,11 @@ public class FinanceRetvBOC
 		return this.financeRetvBO.getCodeMap();
 	}
 
-	public ResponseEntity<String> execlDataFileInsert()
+	public ResponseEntity<String> execlDataFileInsert(final MultipartFile file)
+	{
+		return this.fileUploadBO.uploadExcel(file);
+	}
+	public ResponseEntity<String> uploadExeclDataFile()
 	{
 		return this.financeRetvBO.excelInsert();
 	}
--- aPlatform/src/main/java/com/aPlatform/controller/service/finance/SO/FinancePopSO.java
+++ aPlatform/src/main/java/com/aPlatform/controller/service/finance/SO/FinancePopSO.java
@@ -1,12 +1,16 @@
 package com.aPlatform.controller.service.finance.SO;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.aPlatform.controller.service.finance.BOC.FinanceRetvBOC;
 
@@ -19,11 +23,17 @@ public class FinancePopSO
 	@Autowired
 	FinanceSearchBOC financeSearchBOC;
 
-	@PutMapping(value = "/excelUpload")
+	@PostMapping(value = "/excelUpload", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	@Transactional(rollbackFor = Exception.class, isolation = Isolation.DEFAULT)
-	private ResponseEntity<String> excelFileUpload()
+	private ResponseEntity<String> excelFileUpload(@RequestPart final MultipartFile file)
 	{
-		return this.financeRetvBOC.execlDataFileInsert();
+		return this.financeRetvBOC.execlDataFileInsert(file);
+	}
+	@PostMapping(value = "/excelDataSave")
+	@Transactional(rollbackFor = Exception.class, isolation = Isolation.SERIALIZABLE, propagation = Propagation.NEVER)
+	private ResponseEntity<String> excelDataSave()
+	{
+		return this.financeRetvBOC.uploadExeclDataFile();
 	}
 
 }
