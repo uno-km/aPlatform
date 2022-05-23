7f2eb9a8c0eea0184c25d3730dbc754da275b0b5
uno-kim <zhfldk014745@naver.com>
2022-05-24 00:27:24 +0900

    :wrench: create new function

--- aPlatform/src/main/java/com/aPlatform/controller/file/SO/FileUploadBO.java
+++ aPlatform/src/main/java/com/aPlatform/controller/file/SO/FileUploadBO.java
@@ -1,16 +1,58 @@
 package com.aPlatform.controller.file.SO;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.UUID;
 
-@RestController
-@RequestMapping(value = "/common/file")
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.aPlatform.controller.file.VO.FileInVO;
+
+@Service
 public class FileUploadBO
 {
-	@RequestMapping(value = "/uploadFile")
-	private ModelAndView returnPopupExcelUpload()
+	public ResponseEntity<String> uploadExcel(final MultipartFile file)
 	{
-		return null;
+		String uploadFolder = "C:\\programing\\upload\\fin\\excel";
+		File uploadPath = new File(uploadFolder);
+		if(uploadPath.exists() == false) uploadPath.mkdirs();
+		FileInVO attachDTO = new FileInVO();
+		String uploadFileName = file.getOriginalFilename();
+		uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\") + 1);
+		UUID uuid = UUID.randomUUID();
+		uploadFileName = uuid.toString() + "_" + uploadFileName;
+		try
+		{
+			File saveFile = new File(uploadPath, uploadFileName);
+			file.transferTo(saveFile);
+			attachDTO.setFileRealNm(uploadFileName);
+			attachDTO.setFileChangedNm(uuid.toString());
+			attachDTO.setSavePath(uploadFolder);
+			if(checkImageType(saveFile))
+				attachDTO.setImgYn("Y");
+			else
+				attachDTO.setImgYn("N");
+			return new ResponseEntity<>("200", HttpStatus.OK);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			return new ResponseEntity<>("400", HttpStatus.OK);
+		}
+	}
+	private boolean checkImageType(File file)
+	{
+		try
+		{
+			return Files.probeContentType(file.toPath()).startsWith("image");
+		}
+		catch (IOException e)
+		{
+			return false;
+		}
 	}
 }
