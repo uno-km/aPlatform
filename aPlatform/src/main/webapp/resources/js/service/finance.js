2d98b60365814d6f06a42bf85092541f81c71c14
uno-kim <zhfldk014745@naver.com>
2022-06-06 17:56:00 +0900

    :art: change value

--- aPlatform/src/main/webapp/resources/js/service/finance.js
+++ aPlatform/src/main/webapp/resources/js/service/finance.js
@@ -106,28 +106,28 @@ function finPageInit() {
 }
 //pdf 파일 만드는 버튼 이벤튼
 function getPDFFile() {
-	html2canvas($('#ContentsSectionMain')[0]).then(function(canvas) { //저장 영역 div id
-	    // 캔버스를 이미지로 변환
-	    var imgData = canvas.toDataURL('image/png');
-		     
-	    var imgWidth = 190; // 이미지 가로 길이(mm) / A4 기준 210mm
-	    var pageHeight = imgWidth * 1.414;  // 출력 페이지 세로 길이 계산 A4 기준
-	    var imgHeight = canvas.height * imgWidth / canvas.width;
-	    var heightLeft = imgHeight;
-	    var margin = 10; // 출력 페이지 여백설정
-	    var doc = new jsPDF('p', 'mm');
-	    var position = 0;
-	    // 첫 페이지 출력
-	    doc.addImage(imgData, 'PNG', margin, position, imgWidth, imgHeight);
-	    heightLeft -= pageHeight;
-	    // 한 페이지 이상일 경우 루프 돌면서 출력
-	    while (heightLeft >= 20) {
-	        position = heightLeft - imgHeight;
-	        doc.addPage();
-	        doc.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
-	        heightLeft -= pageHeight;
-	    }
-	    // 파일 저장
-	    doc.save('file-name.pdf');
-	});
+	html2canvas($('#ContentsSectionMain')[0]).then(makePDF(canvas));
 }
+function makePDF(canvas) {
+	// 캔버스를 이미지로 변환
+	let imgData = canvas.toDataURL('image/png');
+	let imgWidth = 200; // 이미지 가로 길이(mm) / A4 기준 210mm
+	let pageHeight = imgWidth * 1.414;  // 출력 페이지 세로 길이 계산 A4 기준
+	let imgHeight = canvas.height * imgWidth / canvas.width;
+	let heightLeft = imgHeight;
+    let margin = 10; // 출력 페이지 여백설정
+    let doc = new jsPDF('p', 'mm');
+    let position = 0;
+    // 첫 페이지 출력
+    doc.addImage(imgData, 'PNG', margin, position, imgWidth, imgHeight);
+    heightLeft -= pageHeight;
+    // 한 페이지 이상일 경우 루프 돌면서 출력
+    while (heightLeft >= 20) {
+        position = heightLeft - imgHeight;
+        doc.addPage();
+        doc.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+        heightLeft -= pageHeight;
+    }
+    doc.save('file-name.pdf');
+}
+
