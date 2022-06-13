76d608846996d50998eb8973c24e7fb9785c581c
uno-kim <zhfldk014745@naver.com>
2022-06-14 00:20:12 +0900

    :wrench: change mapping

--- aPlatform/src/main/java/com/aPlatform/controller/service/finance/SO/FinanceRetvSO.java
+++ aPlatform/src/main/java/com/aPlatform/controller/service/finance/SO/FinanceRetvSO.java
@@ -3,12 +3,15 @@ package com.aPlatform.controller.service.finance.SO;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -50,8 +53,8 @@ public class FinanceRetvSO
 		System.out.println("Client required " + dataform + " data...");
 		return financeSearchBOC.getInfo(financeDataMatrix, dataform, map);
 	}
-	@PostMapping(value = "/addUserInterest")
-	private CommonOutVO insertUserInterestShare(@RequestParam Map<String, Object> param)
+	@PostMapping(value = "/addUserInterest", produces = {MediaType.APPLICATION_JSON_VALUE })
+	private CommonOutVO insertUserInterestShare(@RequestBody Map<String, Object> param)
 	{
 		System.out.println("Client required insertUserInterestShare data...");
 		return financeRetvBOC.insertUserInterestShare(param);
