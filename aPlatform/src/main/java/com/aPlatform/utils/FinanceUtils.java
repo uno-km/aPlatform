d8fa2c5574b21f6d4305fac322f1c73536c9510a
uno-kim <zhfldk014745@naver.com>
2022-11-02 23:15:21 +0900

    :wrench: 금융관련 유틸 파일 수정

--- aPlatform/src/main/java/com/aPlatform/utils/FinanceUtils.java
+++ aPlatform/src/main/java/com/aPlatform/utils/FinanceUtils.java
@@ -4,6 +4,7 @@ public class FinanceUtils
 {
 	public static final String KOSPI = "kospi";
 	public static final String KOSDAQ = "kosdaq";
+	public static final String ALLMARKET = "allmarket";
 	public static final String CODE = "code";
 	public static final String DETAIL_URL = "https://finance.naver.com/item/main.naver?code=";
 	public static final String DETAIL_PHARSETYPE = ".h_th2";
