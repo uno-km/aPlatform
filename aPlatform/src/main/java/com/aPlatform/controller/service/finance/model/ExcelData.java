28a2bbebbbc70b01551484f4bb756e7e459217b7
uno-kim <zhfldk014745@naver.com>
2022-11-06 18:31:17 +0900

    예외처리 기능 구현

--- aPlatform/src/main/java/com/aPlatform/controller/service/finance/model/ExcelData.java
+++ aPlatform/src/main/java/com/aPlatform/controller/service/finance/model/ExcelData.java
@@ -22,40 +22,61 @@ public class ExcelData
 {
 	@Autowired
 	FinanceDataMapper financeDataMapper;
-	public void insertExcelData(CommonOutVO outVO, File file) throws Exception
+	public void insertExcelData(CommonOutVO outVO, File file)
 	{
 		ResultDTO result = new ResultDTO();
 		outVO.setResultDTO(result);
-		XSSFWorkbook excelWorkBook = new XSSFWorkbook(file);
-		for (int sheetIdx = 0; sheetIdx < excelWorkBook.getNumberOfSheets(); sheetIdx++)
+		try
 		{
-			XSSFSheet excelSheet = excelWorkBook.getSheetAt(sheetIdx);
-			// 행 갯수
-			for (int rowIdx = 1; rowIdx < excelSheet.getPhysicalNumberOfRows(); rowIdx++)
+			XSSFWorkbook excelWorkBook = new XSSFWorkbook(file);
+			for (int sheetIdx = 0; sheetIdx < excelWorkBook.getNumberOfSheets(); sheetIdx++)
 			{
-				XSSFRow excelRow = excelSheet.getRow(rowIdx);
-				FinanceVO inVO = new FinanceVO();
-				for (int cellIdx = 0; cellIdx < excelRow.getPhysicalNumberOfCells(); cellIdx++)
+				XSSFSheet excelSheet = excelWorkBook.getSheetAt(sheetIdx);
+				// 행 갯수
+				for (int rowIdx = 1; rowIdx < excelSheet.getPhysicalNumberOfRows(); rowIdx++)
 				{
-					XSSFCell excelCell = excelRow.getCell(cellIdx);
-					if(excelCell != null)
+					XSSFRow excelRow = excelSheet.getRow(rowIdx);
+					FinanceVO inVO = new FinanceVO();
+					for (int cellIdx = 0; cellIdx < excelRow.getPhysicalNumberOfCells(); cellIdx++)
 					{
-						switch (excelCell.getColumnIndex()) {
-							case 2 :
-								inVO.setFinCode(excelCell.getStringCellValue());
-								break;
-							case 4 :
-								inVO.setFinName(excelCell.getStringCellValue());
-								break;
-							case 6 :
-								inVO.setExchange(excelCell.getStringCellValue());
-								break;
+						XSSFCell excelCell = excelRow.getCell(cellIdx);
+						if(excelCell != null)
+						{
+							switch (excelCell.getColumnIndex()) {
+								case 2 :
+									inVO.setFinCode(excelCell.getStringCellValue());
+									break;
+								case 4 :
+									inVO.setFinName(excelCell.getStringCellValue());
+									break;
+								case 6 :
+									inVO.setExchange(excelCell.getStringCellValue());
+									break;
+							}
 						}
 					}
+					this.financeDataMapper.insertSharesInfo(inVO);
 				}
-				this.financeDataMapper.insertSharesInfo(inVO);
 			}
+			result.setCodeMessage("200", "새로운 주식종목등록이 완료되었습니다.");
 		}
-		result.setCodeMessage("200", "새로운 주식종목등록이 완료되었습니다.");
+		catch (FileNotFoundException e)
+		{
+			// TODO: handle exception
+			result.setCodeMessage("500", "파일을 찾을 수 없습니다.");
+			outVO.setError(e.getMessage());
+		}
+		catch (IOException e)
+		{
+			// TODO: handle exception
+			result.setCodeMessage("500", "엑셀파일을 다루는 도중 오류가 발생했습니다..");
+			outVO.setError(e.getMessage());
+		}
+		catch (Exception e)
+		{
+			// TODO: handle exception
+			result.setCodeMessage("500", "알수없는 오류가 발생했습니다. 관리자에게 문의하세요.");
+			outVO.setError(e.getMessage());
+		}
 	}
 }
