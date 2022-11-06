ceca64c7219e1b2c558258756f89ea70be5839d4
uno-kim <zhfldk014745@naver.com>
2022-11-06 18:30:56 +0900

    코드간편화 및 저장로직 변경완료

--- aPlatform/src/main/java/com/aPlatform/controller/service/finance/model/ExcelData.java
+++ aPlatform/src/main/java/com/aPlatform/controller/service/finance/model/ExcelData.java
@@ -1,79 +1,61 @@
 package com.aPlatform.controller.service.finance.model;
 
 import java.io.File;
-import java.text.NumberFormat;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.aPlatform.controller.common.model.CommonOutVO;
+import com.aPlatform.controller.common.model.ResultDTO;
 import com.aPlatform.controller.service.finance.VO.FinanceVO;
+import com.aPlatform.mappers.FinanceDataMapper;
 
 @Service
 @SuppressWarnings("resource")
 public class ExcelData
 {
-	public void insertExcelData(File file) throws Exception
+	@Autowired
+	FinanceDataMapper financeDataMapper;
+	public void insertExcelData(CommonOutVO outVO, File file) throws Exception
 	{
-		List<FinanceVO> outList = new ArrayList<FinanceVO>();
-		XSSFWorkbook workbook = new XSSFWorkbook(file);
-		NumberFormat f = NumberFormat.getInstance();
-		f.setGroupingUsed(false); // 지수로 안나오게 설정
-		// 시트 갯수
-		int sheetNum = workbook.getNumberOfSheets();
-		for (int s = 0; s < sheetNum; s++)
+		ResultDTO result = new ResultDTO();
+		outVO.setResultDTO(result);
+		XSSFWorkbook excelWorkBook = new XSSFWorkbook(file);
+		for (int sheetIdx = 0; sheetIdx < excelWorkBook.getNumberOfSheets(); sheetIdx++)
 		{
-			XSSFSheet sheet = workbook.getSheetAt(s);
+			XSSFSheet excelSheet = excelWorkBook.getSheetAt(sheetIdx);
 			// 행 갯수
-			int rows = sheet.getPhysicalNumberOfRows();
-			for (int r = 1; r < rows; r++)
+			for (int rowIdx = 1; rowIdx < excelSheet.getPhysicalNumberOfRows(); rowIdx++)
 			{
-				XSSFRow row = sheet.getRow(r);
-				int cells = row.getPhysicalNumberOfCells();
+				XSSFRow excelRow = excelSheet.getRow(rowIdx);
 				FinanceVO inVO = new FinanceVO();
-				for (int c = 0; c < cells; c++)
-					if(c == 1 || c == 3 || c == 6)
+				for (int cellIdx = 0; cellIdx < excelRow.getPhysicalNumberOfCells(); cellIdx++)
+				{
+					XSSFCell excelCell = excelRow.getCell(cellIdx);
+					if(excelCell != null)
 					{
-						XSSFCell cell = row.getCell(c);
-						String value = "";
-						if(cell != null)
-							// 타입 체크
-							switch (cell.getCellType())
-							{
-							case STRING :
-							value = cell.getStringCellValue();
-							break;
-							case NUMERIC :
-							value = f.format(cell.getNumericCellValue()) + "";
-							break;
-							case BLANK :
-							value = cell.getBooleanCellValue() + "";
-							break;
-							case ERROR :
-							value = cell.getErrorCellValue() + "";
-							break;
-							default :
-							break;
-							}
-						switch (c) {
-							case 1 :
-								inVO.setFinCode(value);
+						switch (excelCell.getColumnIndex()) {
+							case 2 :
+								inVO.setFinCode(excelCell.getStringCellValue());
 								break;
-							case 3 :
-								inVO.setFinName(value);
+							case 4 :
+								inVO.setFinName(excelCell.getStringCellValue());
 								break;
 							case 6 :
-								inVO.setExchange(value);
-								;
+								inVO.setExchange(excelCell.getStringCellValue());
 								break;
 						}
 					}
-				outList.add(inVO);
+				}
+				this.financeDataMapper.insertSharesInfo(inVO);
 			}
 		}
+		result.setCodeMessage("200", "새로운 주식종목등록이 완료되었습니다.");
 	}
 }
