b727ab071e42d924a67056b735876fb834f65b19
uno-kim <zhfldk014745@naver.com>
2022-11-08 21:37:16 +0900

    :memo: 금융 전용 유틸파일 신규 상수데이터 추가

--- aPlatform/src/main/java/com/aPlatform/utils/FinanceUtils.java
+++ aPlatform/src/main/java/com/aPlatform/utils/FinanceUtils.java
@@ -8,4 +8,6 @@ public class FinanceUtils
 	public static final String CODE = "code";
 	public static final String DETAIL_URL = "https://finance.naver.com/item/main.naver?code=";
 	public static final String DETAIL_PHARSETYPE = ".h_th2";
+	public static final String[] CHART_AREA_ARR = {"day" , "week" , "month3" , "year" , "year3" , "year5" , "year10" };
+	public static final String[] CHART_CANDLE_ARR = {"day" , "week" , "month" };
 }
