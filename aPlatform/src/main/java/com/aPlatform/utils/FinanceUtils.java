328f725dc189f65b7c2c97b176e9fcf4e91eded3
uno-kim <zhfldk014745@naver.com>
2022-11-09 20:38:57 +0900

    :tada: 금융관련 상수 추가

--- aPlatform/src/main/java/com/aPlatform/utils/FinanceUtils.java
+++ aPlatform/src/main/java/com/aPlatform/utils/FinanceUtils.java
@@ -8,6 +8,14 @@ public class FinanceUtils
 	public static final String CODE = "code";
 	public static final String DETAIL_URL = "https://finance.naver.com/item/main.naver?code=";
 	public static final String DETAIL_PHARSETYPE = ".h_th2";
+	public static final String CHART_AREA_IMG_URL = "https://ssl.pstatic.net/imgfinance/chart/item/area/";
+	public static final String CHART_CANDLE_IMG_URL = "https://ssl.pstatic.net/imgfinance/chart/item/candle/";
+	public static final String CHART_COMMON_IMG_URL = ".png?sidcode=1647343521521";
+	public static final String WON_STR = "원";
+	public static final String RANK_STR = "위";
+	public static final String MULTI_STR = "배";
+	public static final String STEADY = "보합";
+
 	public static final String[] CHART_AREA_ARR = {"day" , "week" , "month3" , "year" , "year3" , "year5" , "year10" };
 	public static final String[] CHART_CANDLE_ARR = {"day" , "week" , "month" };
 }
