e2751b2156f16f8b480cc425e79365bf2badc008
uno-kim <zhfldk014745@naver.com>
2022-11-27 21:33:41 +0900

    :tada: create new factory

--- aPlatform/src/main/java/com/aPlatform/controller/service/finance/arch/SetFinanceData.java
+++ aPlatform/src/main/java/com/aPlatform/controller/service/finance/arch/SetFinanceData.java
@@ -1,6 +1,12 @@
 package com.aPlatform.controller.service.finance.arch;
 
+import java.util.HashMap;
+
+import org.jsoup.nodes.Document;
+
+import com.aPlatform.controller.service.finance.VO.FinanceDataMatrix;
+
 public interface SetFinanceData
 {
-
+	public Object excute(FinanceDataMatrix financeDataMatrix, Document doc, HashMap<String, String> outMap, String market, String pharseType);
 }
