9dcad970c4b3836533dfe7a3e3e7beb4414d0c65
uno-kim <zhfldk014745@naver.com>
2022-11-29 23:11:54 +0900

    :tada: create Super java File

---
+++ aPlatform/src/main/java/com/aPlatform/controller/service/finance/arch/SuperFinance.java
@@ -0,0 +1,12 @@
+package com.aPlatform.controller.service.finance.arch;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+public abstract class SuperFinance
+{
+	public Elements contents;
+	public String[] parsingContainer;
+	public String market;
+	public Document doc;
+}
