47b4e52e5ce93afc75badd3df0fd8eb936eef250
uno-kim <zhfldk014745@naver.com>
2022-12-01 18:41:38 +0900

    상수다루는 인터페이스 HtmlUtils이름변경 및 값 몇개 더 추가

---
+++ aPlatform/src/main/java/com/aPlatform/utils/HtmlConstants.java
@@ -0,0 +1,18 @@
+package com.aPlatform.utils;
+
+public interface HtmlConstants
+{
+	public static final String TD = "td";
+	public static final String TR = "tr";
+	public static final String TH = "th";
+	public static final String TBODY = "tbody";
+	public static final String EM = "em";
+	public static final String SPAN = "span";
+	public static final String LI = "li";
+	public static final String TABLE = "table";
+	public static final String STRONG = "strong";
+	public static final String THEAD = "thead";
+	public static final String HREF = "href";
+	public static final String A = "a";
+	
+}
