f9de61589a86add1a3cb1fdb082c80979a8fba54
uno-kim <zhfldk014745@naver.com>
2022-12-13 00:02:07 +0900

    :wrench: update test page

--- aPlatform/src/main/java/com/aPlatform/controller/main/SO/APITestSO.java
+++ aPlatform/src/main/java/com/aPlatform/controller/main/SO/APITestSO.java
@@ -3,6 +3,7 @@ package com.aPlatform.controller.main.SO;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -38,8 +40,13 @@ public class APITestSO
 		return null;
 	}
 	
+	// @PostMapping(value = "/file")
+	// private String postFile(@RequestParam("file") final MultipartFile file)
+	// {
+	// return null;
+	// }
 	@PostMapping(value = "/file")
-	private String postFile(@RequestParam final Map<String, String> param, @RequestParam("file") final MultipartFile file) throws Exception
+	private String postFile(@RequestParam final Map<String, String> param, @RequestParam("file") final MultipartFile file)
 	{
 		return null;
 	}
@@ -47,10 +54,18 @@ public class APITestSO
 	@PostMapping(value = "/filePart")
 	private String postPartFile(
 			@RequestParam final Map<String, String> param
-			, @RequestPart final MultipartFile file
-			)
+			, @RequestPart final MultipartFile file)
 	{
-		return null;
+		/*대강 파일 업로드 성공은 200, 실패는 400 코드를 클라이언트로 보내겠습니다.*/
+		if (file.getSize() > 0)
+		{
+			/* 파일을 업로드하는 방식은 생략하겠습니다. */
+			return "200";
+		}
+		else
+		{
+			return "400";
+		}
 	}
 	
 	@PutMapping(value = "/file")
@@ -60,6 +75,33 @@ public class APITestSO
 	}
 	@PatchMapping(value = "/file")
 	private String patchFile(@RequestPart final MultipartFile file) throws Exception
+	{
+		return null;
+	}
+	// @GetMapping(value = "fetch")
+	// private String getFetch()
+	// {
+	// return null;
+	// }
+	// @GetMapping(value = "fetch")
+	// private String getFetch(@RequestParam final Map<String, String> param)
+	// {
+	// return null;
+	// }
+	@GetMapping(value = "fetch")
+	@ResponseBody
+	private Map<String, String> getFetch(@RequestParam final Map<String, String> param)
+	{
+		return param;
+	}
+	// @GetMapping(value = "fetch")
+	// private Map<String, String> getFetch(
+	// @RequestParam final Map<String, String> param)
+	// {
+	// return param;
+	// }
+	@GetMapping(value = "fetch/{name}/{age}")
+	private String getPathFetch(@PathVariable(value = "name") String name, @PathVariable(value = "age") String age)
 	{
 		return null;
 	}
--- aPlatform/src/main/webapp/resources/js/test/FileTest.js
+++ aPlatform/src/main/webapp/resources/js/test/FileTest.js
@@ -4,40 +4,63 @@ function addEventFile()
 {
 		//document.getElementById('get').addEventListener('click', _get);
 		document.getElementById('post').addEventListener('click', _post);
-		document.getElementById('put').addEventListener('click', _put);
-		document.getElementById('patch').addEventListener('click', _patch);
+		//document.getElementById('put').addEventListener('click', _put);
+		//document.getElementById('patch').addEventListener('click', _patch);
 }
 function _get()
 {
-	let inFile = document.getElementById('getFile').files[0];
-	let inData = new FormData();
-	inData.append('file', inFile);
-	$.ajax({
-  		type: "get"
-  		, url: "file"
-  		, data : inData
-  		, success: function(){}
-	});  
+	let inData = {
+		name	:	'unokim'
+		,age	:	28
+	}
+	let param = new URLSearchParams(inData).toString();
+	fetch("fetch?" + param)
+	.then((rs) => {
+		return rs.json()
+	})
+	.then((data)=>{
+		console.log(data);
+	});
 }
 function _post()
 {
 	let inFile = document.getElementById('postFile').files[0];
 	let inData = new FormData();
-	inData.append('file', inFile);
-	inData.append('auth', 'master');
+	
+	inData.append('auth', 'manager');
 	inData.append('type', 'manual');
-	$.ajax({
-  		type: "post"
-  		, url: "filePart"
-		, processData : false
-		, contentType : false
-  		, data : inData
-		, dataType : 'JSON'
-  		, success: function(){}
-	});  
+	inData.append('targetSystem', 'naver-map');
+	inData.append('file', inFile);
+		
+	fetch(	'filePart'
+			, {
+				method	: "POST"
+				,body	: inData
+				,headers: {}
+	})
+	.then((rs) => {
+		if(rs.ok)
+		{
+			console.log('통신성공');
+			return rs.json()
+		}
+	})
+	.then((data) => {
+		if(data.toString()==="200")
+		{
+			alert("파일 업로드에 성공했습니다.");
+		}
+		else
+		{
+			alert("실패");
+		}
+	});
 }
 function _put()
 {
+inData.append('file', inFile);
+
+
 	let inFile = document.getElementById('putFile').files[0];
 	let inData = new FormData();
 	inData.append('file', inFile);
