3b16ac3a3e2f31b20d5aea070f88901306220528
zhfld <zhfld@LAPTOP-AFG5HTSP>
2023-02-09 23:37:17 +0900

    추상화 팩토리패턴 준비

---
+++ aPlatform/src/main/java/com/aPlatform/controller/service/finance/model/FinanceDataTypeFacotry.java
@@ -0,0 +1,28 @@
+package com.aPlatform.controller.service.finance.model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+@Service
+public class FinanceDataTypeFacotry
+{
+	private Map<String, FinanceDataType> finDtTypeCache = new HashMap<>();
+	@Autowired
+	FinanceDataTypeFacotry()
+	{
+		List<FinanceDataType> financeDataTypes = new ArrayList<>();
+		for (FinanceDataType financeDataType : financeDataTypes)
+		{
+			this.finDtTypeCache.put(financeDataType.getType(), financeDataType);
+		}
+	}
+	
+	public FinanceDataType getFinDataType(String type)
+	{
+		return finDtTypeCache.get(type);
+	}
+}
--- aPlatform/src/main/java/com/aPlatform/controller/service/finance/model/GetURLInfo.java
+++ aPlatform/src/main/java/com/aPlatform/controller/service/finance/model/GetURLInfo.java
@@ -1,8 +1,4 @@
 package com.aPlatform.controller.service.finance.model;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 import org.springframework.stereotype.Service;
 
 import com.aPlatform.controller.service.finance.VO.FinanceDataMatrix;
@@ -11,23 +7,16 @@ public class GetURLInfo
 {
 	public Object getUrlInfoObject(final FinanceDataMatrix financeDataMatrix, final String url, final String pharseType) throws Exception
 	{
+		this.asd(financeDataMatrix, url, pharseType);
 		return FinanceURL.pharsingURL(financeDataMatrix, url, pharseType);
 	}
-	public Object getUrlInfoAllObject(final FinanceDataMatrix financeDataMatrix, final Map<String, Object> map) throws Exception
+	public Object asd(FinanceDataMatrix financeDataMatrix, String url, String pharseType)
 	{
-		// 여기에 맵을 다루는 로직을 만든다
-		UrlMileStone[] urlArr = UrlMileStone.values();
-		ArrayList<String> outMapKeys = new ArrayList<String>();
-		LinkedHashMap<String, Object> outMap = new LinkedHashMap<String, Object>();
-		for (UrlMileStone urls : urlArr)
-		{
-			outMapKeys.add(urls.name());
-			// outMap.put(urls.name(), FinanceURL.pharsingURL(financeDataMatrix, urls.name(), pharseType));
-		}
-		for (int i = 0; i < urlArr.length; i++)
-		{
-			// outMap.put("Str", FinanceURL.pharsingURL(financeDataMatrix, url, pharseType));
-		}
-		return outMap;
+		financeDataMatrix.setMarket(url);
+		financeDataMatrix.setPharseType(pharseType);
+		financeDataMatrix.setConnectedDoc(financeDataMatrix.getPageDOCMap().get(url));
+		FinanceDataType financeDataType = new FinanceDataTypeFacotry().getFinDataType(pharseType);
+		Object qw = financeDataType.excute(financeDataMatrix);
+		return null;
 	}
 }
